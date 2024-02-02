362befadf945218b09c6ae1a09f673f534b447e9
Barbi Adkins <badkins0043@gmail.com>
2024-02-02 18:02:38 -0500

    test

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -2,7 +2,7 @@ SELECT id AS 'Movie_ID',
 	name AS 'Movie_Title',
 	imdb_rating AS 'Rating'
 FROM movies
-WHERE year <1986
+WHERE year < 1986
 AND genre = 'horror'
-Order BY imdb_rating Despicable
+Order BY imdb_rating Desc
 LIMIT 3;
